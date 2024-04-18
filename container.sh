797854a02b368b2c9a584741cbd90828301cb9ae
adarika <ADari.Ka@yandex.ru>
2024-04-18 21:55:36 +0300

    ref+add: complete container script with delete func

--- container.sh
+++ container.sh
@@ -5,11 +5,25 @@ ROOTFS_PATH=./shared/rootfs.tar
 
 MOUNT_PATH=$V_PATH/mnt/$1
 
+image=$V_PATH/$1.img
+
+loop_device=''
+
+if [[ -z $1 ]]; then
+    echo "invalid (empty) name '$1' for container!!"
+    exit 1
+fi
+
+if [[ -f $image_path ]]; then
+    echo "the container with name '$1' already exists :("
+    exit 1
+fi
+
+
 virt_storage_device () {
     # create virtual storage device using file as image
     # it will contain file system info and other data
-    local image=$V_PATH/$1.img
- 
+
     local size=1G
 
     # create a  zeroed image file -> loop device
@@ -33,6 +47,13 @@ virt_storage_device () {
 }
 
 
+delete () {
+    # unmount + remove loop_device
+    umount $loop_device
+    losetup -D $loop_device
+    rm -rf $image $MOUNT_PATH
+}
+
 virt_storage_device $1  # name
 
 setup_path="export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
@@ -49,3 +70,6 @@ isolation_cmd="unshare -n -p -f"
 
 # run
 cgexec -g "$cgroups:$1" $isolation_cmd chroot $MOUNT_PATH /bin/bash -c "$full_cmd" || true
+
+# rm after complete container
+delete $1
